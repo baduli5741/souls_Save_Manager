810f962203e1a7bfc64c9657dc2714f702cbdc5c
123 <123@DESKTOP-B3RQ93M>
2023-08-22 13:14:34 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace darksouls3_Save_Manager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFind = new System.Windows.Forms.Button();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.btnDuplicate = new System.Windows.Forms.Button();
+            this.lvFileList = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // btnFind
+            // 
+            this.btnFind.Location = new System.Drawing.Point(86, 347);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(115, 30);
+            this.btnFind.TabIndex = 0;
+            this.btnFind.Text = "Find my save";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(231, 347);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(305, 25);
+            this.txtFilePath.TabIndex = 1;
+            // 
+            // btnDuplicate
+            // 
+            this.btnDuplicate.Location = new System.Drawing.Point(616, 348);
+            this.btnDuplicate.Name = "btnDuplicate";
+            this.btnDuplicate.Size = new System.Drawing.Size(75, 29);
+            this.btnDuplicate.TabIndex = 2;
+            this.btnDuplicate.Text = "Duplicate";
+            this.btnDuplicate.UseVisualStyleBackColor = true;
+            this.btnDuplicate.Click += new System.EventHandler(this.btnDuplicate_Click);
+            // 
+            // lvFileList
+            // 
+            this.lvFileList.HideSelection = false;
+            this.lvFileList.Location = new System.Drawing.Point(562, 92);
+            this.lvFileList.Name = "lvFileList";
+            this.lvFileList.Size = new System.Drawing.Size(184, 204);
+            this.lvFileList.TabIndex = 3;
+            this.lvFileList.UseCompatibleStateImageBehavior = false;
+            this.lvFileList.View = System.Windows.Forms.View.Details;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lvFileList);
+            this.Controls.Add(this.btnDuplicate);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.btnFind);
+            this.Name = "Form1";
+            this.Text = "ds3 save manager";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Button btnDuplicate;
+        private System.Windows.Forms.ListView lvFileList;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace darksouls3_Save_Manager
+{
+    public partial class Form1 : Form
+    {
+        private string userDirectory;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            userDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "DarkSouls3");
+            btnDuplicate.Visible = false;
+            lvFileList.Columns.Add("File Name", 150);
+            lvFileList.Columns.Add("Change Time", 150);
+
+            PopulateFileList();
+        }
+        private void PopulateFileList()
+        {
+            lvFileList.Items.Clear();
+
+            string saveDirectory = Path.Combine(Application.StartupPath, "save");
+
+            if (Directory.Exists(saveDirectory))
+            {
+                string[] files = Directory.GetFiles(saveDirectory);
+
+                foreach (string filePath in files)
+                {
+                    FileInfo fileInfo = new FileInfo(filePath);
+                    ListViewItem item = new ListViewItem(fileInfo.Name);
+                    item.SubItems.Add(fileInfo.LastWriteTime.ToString("yyyy-MM-dd HH:mm:ss"));
+                    lvFileList.Items.Add(item);
+                }
+            }
+        }
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!Directory.Exists(userDirectory))
+                {
+                    MessageBox.Show("The 'DarkSouls3' directory does not exist.\nYou should execute DarkSouls3 at least once", "Directory Not Found", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+
+                string targetFileName = "DS30000.txt";
+                string[] files = Directory.GetFiles(userDirectory, targetFileName, SearchOption.AllDirectories);
+
+                if (files.Length > 0)
+                {
+                    txtFilePath.Text = "File found at:";
+                    foreach (string filePath in files)
+                    {
+                        txtFilePath.Text += Environment.NewLine + filePath;
+                    }
+
+                    btnDuplicate.Visible=true;
+                    PopulateFileList();
+                }
+                else
+                {
+                    txtFilePath.Text = "File not found.";
+                }
+            }
+            catch (Exception ex)
+            {
+                txtFilePath.Text = "Error: " + ex.Message;
+            }
+        }
+
+        private void btnDuplicate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string sourceFile = txtFilePath.Text.Split('\n')[1].Trim(); // 첫 번째 파일 경로 추출
+                string targetDirectory = Path.GetDirectoryName(Application.ExecutablePath); // 실행 파일이 있는 폴더
+
+                string newFileName = Path.GetFileNameWithoutExtension(sourceFile) + "_" + DateTime.Now.ToString("yyyyMMddHHmmss") + Path.GetExtension(sourceFile);
+                string targetFile = Path.Combine(targetDirectory, "save", newFileName); // 타겟 파일 경로 생성
+
+                Directory.CreateDirectory(Path.Combine(targetDirectory, "save")); // save 폴더 생성
+                File.Copy(sourceFile, targetFile); // 파일 복사
+
+                MessageBox.Show("File duplicated successfully.", "Duplicate Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                PopulateFileList();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error duplicating file: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+    }
+}
