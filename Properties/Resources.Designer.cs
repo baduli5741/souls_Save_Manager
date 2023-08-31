2429eaf7fdaf07a3b8d4decd797837da6450ce23
baduli5741 <baduli5741@gmail.com>
2023-08-31 15:14:02 +0900

    openssl이용 + 이름변경

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace darksouls3_Save_Manager.Properties {
+namespace souls_Save_Manager.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace darksouls3_Save_Manager.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("darksouls3_Save_Manager.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("souls_Save_Manager.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace darksouls3_Save_Manager.Properties {
+namespace souls_Save_Manager.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
